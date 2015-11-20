aa100ee7353f2f058014f851d1491044e6ae1bd6
Sebastain Misas <juanmisas2@gmail.com>
2015-11-20 15:44:00 -0500

    added preview music on click on the play button

--- main.js
+++ main.js
@@ -12,18 +12,50 @@ $(document).on("ready", function () {
 		};
 
 
-
+		$('.container').empty()
 		$.ajax({
 			url: "https://api.spotify.com/v1/search?type=track&query=" + user_input(),
 			success: function (result){
+				$('.container').empty()
 				result.tracks.items.forEach(function (track){
-					$(".title").text(track.name);
-					$(".artist").text(track.artists[0].name);
-					$(".track-cover").attr("src", track.album.images[0].url);
+					var tracks_frame = '<div class="widget js-player-wrapper">\
+											<div class="header">\
+												<div class="btn-play disabled"></div>\
+												<div class="metadata">\
+													 <p class="title">' + track.name + '</p>\
+													 <p class="artist">' + track.artists[0].name + '</p>\
+															\
+													 <div class="seekbar">\
+													   <progress value="5" max="30"></progress>\
+													 </div>\
+												</div>\
+											</div>\
+											<div class="cover">\
+												<img class="track-cover" src="'+ track.album.images[0].url +'">\
+											</div>\
+													\
+											<audio class="js-player" src="'+ track.preview_url +'"></audio>\
+										</div><br>';
+
+					$('.container').append(tracks_frame);
+					
 				});
+
+					$(".btn-play").on("click", function (event) {
+						var button = $(event.currentTarget)
+						var audio = button.closest(".js-player-wrapper").find(".js-player")
+				        if (button.hasClass("playing")) {
+				           button.toggleClass('playing')
+				           audio.trigger('pause');
+				        }
+				       	else {
+				       		button.toggleClass('playing')
+				            audio.trigger('play');
+				       	};
+											  
+					});
 			},
-			error: errors,
-		})
-		
+		error: errors,
+		});
 	});
 });
